13797a292a4373520b346c1d18dcfd62196c01cf
christian <ch.ov@outlook.com>
2022-08-14 10:28:40 +0000

    1-binary.c 108-O

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -8,9 +8,36 @@
  */
 int binary_search(int *array, size_t size, int value)
 {
-	size_t x = 0;
+	size_t x, y = 0, low = 0, high = size - 1;
 
 	if (array == NULL)
 		return (-1);
 
+	while (low <= high)
+	{
+		y = (low + high) / 2;
+		printf("Searching in array: ");
+
+		x = low;
+		while (x <= high)
+		{
+			printf("%d", array[x]);
+			if (x < high)
+				printf(", ");
+			x++;
+		}
+
+		printf("\n");
+	
+		if (array[y] == value)
+			return (y);
+
+		else if (array[y] < value)
+			low = y + 1;
+
+		else
+			high = y - 1;
+	}
+
+	return (-1);
 }
